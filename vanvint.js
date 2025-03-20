8374e1b8f6132fda9d44a9ca4ca8c95bdff673dc
Juanluis439 <juanluisdelacruz439@gmail.com>
2025-03-20 09:53:13 +0100

    Corrección del enlace de WhatsApp

--- vanvint.js
+++ vanvint.js
@@ -1,5 +1,5 @@
 function enviarWhatsApp(producto) {
-  const telefono = "wa.me/+393409691101";
+  const telefono = "+393409691101"; // Número sin 'wa.me/'
   const mensaje = "¡Hola! Estoy interesado en comprar el producto: " + producto;
   const enlaceWhatsApp =
     "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
