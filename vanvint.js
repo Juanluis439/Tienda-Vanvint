5e5a2cff9c96b1945469009d958c2402831a6639
Juanluis439 <juanluisdelacruz439@gmail.com>
2025-04-16 11:20:59 +0200

    funciones de carrito, wsp, imagenes agregadas

---
+++ vanvint.js
@@ -0,0 +1,21 @@
+function enviarWhatsApp() {
+  const telefono = "+393409691101"; // Número sin 'wa.me/'
+
+  // Crear mensaje con la información del carrito
+  let mensaje =
+    "¡Hola! Estoy interesado en comprar los siguientes productos:\n\n";
+
+  carrito.forEach((producto) => {
+    mensaje += `Nombre: ${producto.nombre}\n`;
+    mensaje += `Cantidad: ${producto.cantidad}\n`;
+    mensaje += `Precio: €${producto.precio.toFixed(2)}\n`;
+    mensaje += `Imagen: ${producto.img}\n\n`; // Puedes modificar esta línea si quieres mostrar la imagen o solo el nombre del archivo
+  });
+
+  // Crear el enlace de WhatsApp con el mensaje
+  const enlaceWhatsApp =
+    "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje);
+
+  // Redirigir al enlace de WhatsApp
+  window.location.href = enlaceWhatsApp;
+}
