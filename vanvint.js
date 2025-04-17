5fd4f2692d034f6e65dd3a32ebfd8edd6ce0f7af
Juanluis439 <juanluisdelacruz439@gmail.com>
2025-04-17 11:16:31 +0200

    Correcion de mensaje de wsp

--- vanvint.js
+++ vanvint.js
@@ -5,10 +5,10 @@ function enviarWhatsApp() {
   let mensaje = "Ciao! Sono interessato ad acquistare i seguenti prodotti:\n\n";
 
   carrito.forEach((producto) => {
-    mensaje += `Nombre: ${producto.nombre}\n`;
-    mensaje += `Cantidad: ${producto.cantidad}\n`;
-    mensaje += `Precio: €${producto.precio.toFixed(2)}\n`;
-    mensaje += `Imagen: ${window.location.origin}/${producto.img}\n\n`; // Puedes modificar esta línea si quieres mostrar la imagen o solo el nombre del archivo
+    mensaje += `Nome: ${producto.nombre}\n`;
+    mensaje += `Quantità: ${producto.cantidad}\n`;
+    mensaje += `Prezzo: €${producto.precio.toFixed(2)}\n`;
+    mensaje += `Immagine: ${window.location.origin}/${producto.img}\n\n`; // Puedes modificar esta línea si quieres mostrar la imagen o solo el nombre del archivo
   });
 
   // Crear el enlace de WhatsApp con el mensaje
